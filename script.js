6842ab615159e27aa86bcf2ee1d9e79b137aaf4e
SUDEEP1221 <88239864+SUDEEP1221@users.noreply.github.com>
2022-12-09 20:11:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1475961953977596"
-     crossorigin="anonymous"></script>
-
 const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
